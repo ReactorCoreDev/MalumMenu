6ef1512e99b6b0e503811579c4a92d9ae61a3624
ReactorCoreDev <165254059+ReactorCoreDev@users.noreply.github.com>
2025-11-19 11:39:42 +0100

    Update PlayerPhysicsPatches.cs

--- src/Patches/PlayerPhysicsPatches.cs
+++ src/Patches/PlayerPhysicsPatches.cs
@@ -19,7 +19,7 @@ public static class PlayerPhysics_LateUpdate
         MalumCheats.killAllImpsCheat();
         MalumCheats.teleportCursorCheat();
         MalumCheats.completeMyTasksCheat();
-        MalumCheats.crashEveryoneOHMYPC
+        MalumCheats.crashEveryoneOHMYPC();
 
         MalumPPMCheats.spectatePPM();
         MalumPPMCheats.killPlayerPPM();
